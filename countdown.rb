9e60ff3085122dae8cf82e49dfe4cba50e51bf7b
Nicholas Dover <dovern42@gmail.com>
2019-02-03 17:30:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,9 +3,10 @@
 def countdown(number)
   count = number
   while count > 0
-    puts "#{count} #{count = 1 ? "SECOND" : "SECONDS"}!"
+    puts "#{count} #{count == 1 ? "SECOND" : "SECONDS"}!"
     count += 1
   end
+  "HAPPY NEW YEAR!"
 end
 
 countdown(5)
