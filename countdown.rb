10a5ec29e50d54edb0a1637fca320c5cfeb0949c
Nicholas Dover <dovern42@gmail.com>
2019-02-03 17:28:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  count = number
+  while count > 0
+    puts "#{count} #{count = 1 ? "SECOND" : "SECONDS"}!"
+  end
 end
+
+countdown(5)
