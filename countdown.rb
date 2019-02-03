d2ec1c48a21a6e3320145558d1e8fbb071bd804d
Nicholas Dover <dovern42@gmail.com>
2019-02-03 17:35:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@
 def countdown(number)
   count = number
   while count > 0
-    puts "#{count} SECOND(S)!"
+    "#{count} SECOND(S)!"
     count -= 1
   end
   "HAPPY NEW YEAR!"
@@ -12,7 +12,7 @@ end
 def countdown_with_sleep(number)
   count = number
   while count >0
-    puts "#{count} SECOND(S)!"
+    "#{count} SECOND(S)!"
     count -= 1
     sleep(1)
   end
